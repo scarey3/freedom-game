1d93fb8587340dbeddb18fead8fe04be9881fb86
AvinaDiviri <violetpfeiffer@gmail.com>
2015-11-25 12:35:37 -0500

    Update freedomGame.c

--- freedomGame.c
+++ freedomGame.c
@@ -1,1 +1,13 @@
-# freedom-game
+#include <stdio.h>
+#define ROWS 10
+#define COLUMNS 10
+
+/*Samuel Carey, Adam Capar, Violet Pfeiffer 11/25/15
+*CIS 190 Project 3
+*/
+
+/*****************Main***************/
+int main(){
+  
+  	return 0;
+}

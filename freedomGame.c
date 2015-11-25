89fcac1d70ec196142effc71e860257804995c35
Samuel Carey <scarey3@umassd.edu>
2015-11-25 13:12:12 -0500

    included header files and some functions

--- freedomGame.c
+++ freedomGame.c
@@ -1,13 +1,25 @@
+/* Violet Pfeiffer, Adam Capar, Samuel Carey
+ * Project 3
+ * 25 November 2015
+ * Freedom Game. An "n-in-a-row" board game.
+ */
+
 #include <stdio.h>
+#include "freedomMoves.h"
+#include "freedomScoring.h"
+
 #define ROWS 10
 #define COLUMNS 10
 
-/*Samuel Carey, Adam Capar, Violet Pfeiffer 11/25/15
-*CIS 190 Project 3
-*/
+void printBoard(char board[][]);
 
-/*****************Main***************/
-int main(){
-  
+int main() {
+    char boardArray[ROWS][COLUMNS];
+    
   	return 0;
+}
+
+// TODO: Function will print the board.
+void printBoard(char board[][]) {
+
 }

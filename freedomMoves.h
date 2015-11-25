0692d9e19f1d2bc1b271468df4ffb9ce9154763d
Samuel Carey <scarey3@umassd.edu>
2015-11-25 12:53:18 -0500

    added header files

--- freedomMoves.h
+++ freedomMoves.h
@@ -1,1 +1,13 @@
-# freedom-game
+/* Violet Pfeiffer, Adam Capar, Samuel Carey
+ * Project 3
+ * 25 November 2015
+ * 
+ */
+
+#include <stdio.h>
+
+#ifndef FREEDOMMOVES_H
+    #define FREEDOMMOVES_H
+    // Function declarations go here.
+    
+#endif
--- freedomScoring.h
+++ freedomScoring.h
@@ -1,1 +1,13 @@
-# freedom-game
+/* Violet Pfeiffer, Adam Capar, Samuel Carey
+ * Project 3
+ * 25 November 2015
+ * 
+ */
+
+#include <stdio.h>
+
+#ifndef FREEDOMSCORING_H
+    #define FREEDOMSCORING_H
+    // Function declarations go here.
+    
+#endif

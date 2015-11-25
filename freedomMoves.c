c3c3899f92bb812c507a5d8c767629269340776d
Samuel Carey <scarey3@umassd.edu>
2015-11-25 12:58:12 -0500

    added freedomScoring.c and freedomMoves.c

--- freedomMoves.c
+++ freedomMoves.c
@@ -1,1 +1,9 @@
-# freedom-game
+/* Violet Pfeiffer, Adam Capar, Samuel Carey
+ * Project 3
+ * 25 November 2015
+ * 
+ */
+
+#include "freedomMoves.h"
+
+// Functions go here.
--- freedomScoring.c
+++ freedomScoring.c
@@ -1,1 +1,9 @@
-# freedom-game
+/* Violet Pfeiffer, Adam Capar, Samuel Carey
+ * Project 3
+ * 25 November 2015
+ * 
+ */
+
+#include "freedomScoring.h"
+
+// Functions go here.

20e5d57cbe0fb48cff81837130b99920aae33365
AvinaDiviri <AvinaDiviri@users.noreply.github.com>
2015-12-04 18:24:17 -0500

    Update freedomGame.c

--- freedomGame.c
+++ freedomGame.c
@@ -23,7 +23,11 @@ int getSize();
 int main() {
     char **board;
     int size = getSize();
-    
+    int coordinates[cBUFFER]; /*holds converted coordinates (row, column) of playerMove*/
+    int turn = 0; /*what turn game is currently at*/
+	int valid = 0; /*if move is valid*/
+	int player = 1; /*which player*/
+	
     board = allocateBoard(size);
     clearBoard(board, size);
     printBoard(board, size);

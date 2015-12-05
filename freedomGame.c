af3f8fff8bd131feee7db686bc18e02fb8cd200e
Violet <violetpfeiffer@gmail.com>
2015-12-05 13:26:14 -0500

    added playerMove and related functions

--- freedomGame.c
+++ freedomGame.c
@@ -10,16 +10,50 @@
 
 #define ROWS 10
 #define COLUMNS 10
+#define pmBUFFER 4
+#define cBUFFER 3
+/*prototype declarations*/
+void printBoard(char board[][COLUMNS]);
 
-void printBoard(char board[][]);
-
+/*****************Main***************/
 int main() {
-    char boardArray[ROWS][COLUMNS];
-    
+    char board[ROWS][COLUMNS]; /*board*/
+    char playerMove[pmBUFFER] = "\0\0\0\0"; /*holds player coordinates*/
+	int coordinates[cBUFFER]; /*holds converted coordinates (row, column) of playerMove*/
+	int turn = 0;
+	int valid = 0;
+	int player = 2;
   	return 0;
 }
-
+/*******************Functions********************/
+/*game logic*/
+void playGame(void){
+	char newGame = 'Y'; /*if player wants to play new game*/
+	char playerMove[pmBUFFER]; /*holds player move*/
+	int coordinates[cBUFFER]; /*holds converted coordinates (row, column) of playerMove*/
+	
+	
+	while(newGame == 'Y'|| newGame =='y'){
+		/*print board*/
+		//printBoard(board[][COLUMNS]);
+		
+		/*welcome message. Get user input*/
+		printf("Welcome to Freedom! White goes first! Please choose a space.\n");
+		getPlayerMove(playerMove);
+		
+		/*convert user input*/
+		
+		/*validate user input*/
+		
+		/*place piece on board*/
+		
+		/*change player turn*/
+		
+	}	
+	
+}
 // TODO: Function will print the board.
-void printBoard(char board[][]) {
+void printBoard(char board[][COLUMNS]) {
 
 }
+
--- freedomMoves.c
+++ freedomMoves.c
@@ -3,7 +3,70 @@
  * 25 November 2015
  * 
  */
-
 #include "freedomMoves.h"
 
-// Functions go here.
+
+
+/*******************Functions********************/
+/*Retrieves and returns the player's input*/
+void getPlayerMove(char playerMove[]){
+
+	/*Get user input*/
+	fgets(playerMove, pmBUFFER, stdin);
+	//printf("%c %c %c %c\n", playerMove[0], playerMove[1], playerMove[2],playerMove[3]);
+	
+	return;
+}
+
+/*convert user input to tile position*/
+void convertPlayerMove(char playerMove[], int coordinates[]){
+	
+	/*convert row from capital char to int*/
+	coordinates[0] = playerMove[0] - 'A'; //<--offset necesary to convert to ints
+	
+	/*checks if second coordinate is 10*/
+	if(playerMove[1] == '1' && playerMove[2]== '0'){
+		//printf("converting 10 to 9\n");
+		playerMove[1] = '9';
+		coordinates[1] = playerMove[1] - '0'; //<-- offset adjusted for 10
+	}
+	else{
+		/*convert column from numerical char to int*/
+		coordinates[1] = playerMove[1] - '1'; //<-- offset isn't '0' because board numbers are offset by 1
+	}
+	
+	return;
+}
+
+/*check if tile position is valid. Return 1 if true, 0 if false*/
+int isValid(char playerMove[], int turn){//FIX ME: under construction
+
+	/*if both elements of playerMove are valid return 1*/
+	if(playerMove[0] >= 'A' &&  playerMove[0] <= 'J'){
+		if(playerMove[1] >= '0' && playerMove[1] <= '9'){ //FIX ME: can't have multicharacter char
+			if(turn == 0){
+				return 1;
+			}
+			
+		}
+	}
+	/*if both elements of playerMove are NOT valid return 0*/
+	return 0;
+}
+
+/*place piece on tile position*/
+void implementPlayerMove(int player, int coordinates[], char board[][COLUMNS]){
+	
+	/*check if player1 (white)*/
+	if(player == 1){
+		/*place white piece*/
+		board[coordinates[0]][coordinates[1]] = 'W';
+	}
+	/*check if player2 (black)*/
+	else if(player == 2){
+		/*place black piece*/
+		board[coordinates[0]][coordinates[1]] = 'B';
+	}
+	printf("%c\n", board[coordinates[0]][coordinates[1]]);
+	return;
+}

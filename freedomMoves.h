632cb04560766dee34cd5bc6dca454cd2fbac5b8
AvinaDiviri <AvinaDiviri@users.noreply.github.com>
2015-12-06 13:57:53 -0500

    deleted coordinate buffer and old function headers

--- freedomMoves.h
+++ freedomMoves.h
@@ -6,14 +6,10 @@
 
 #include <stdio.h>
 #define pmBUFFER 4 /*for playerMove buffer*/
-#define cBUFFER 3 /*coordinates buffer*/
 
 #ifndef FREEDOMMOVES_H
     #define FREEDOMMOVES_H
     // Function declarations go here.
-    void getPlayerMove(char playerMove[]);
-    void convertPlayerMove(char playerMove[], int coordinates[]);
-    int isValid(char playerMove[], int turn);
-    void implementPlayerMove(int player, int coordinates[], char board[][COLUMNS]);
+    
     
 #endif

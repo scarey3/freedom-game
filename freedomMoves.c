268d08ac58db53c7558dbe34b996e56a99b1844e
Violet <violetpfeiffer@gmail.com>
2015-12-06 23:42:40 -0500

    fixed row always being invalid

--- freedomMoves.c
+++ freedomMoves.c
@@ -13,7 +13,7 @@ void getPlayerMove(char playerMove[]){
 	/*Clear buffer. Get user input*/
 	fflush(stdin);
 	fgets(playerMove, pmBUFFER, stdin);
-	printf("%c %c %c %c\n", playerMove[0], playerMove[1], playerMove[2],playerMove[3]);
+	//printf("%c %c %c %c\n", playerMove[0], playerMove[1], playerMove[2],playerMove[3]);
 	
 	/*if column is not lowercase OR uppercase char*/
 	/*if(!((playerMove[0] >= 'a' && playerMove[0] <= 'z') || (playerMove[0] >= 'A' && playerMove[0] <= 'Z'))){
@@ -22,16 +22,16 @@ void getPlayerMove(char playerMove[]){
 	}
 	
 	/*row is not a single digit*/
-	if(!(playerMove[1] >= '1' && playerMove[1] <= '9')){
+	/*if(!(playerMove[1] >= '1' && playerMove[1] <= '9')){
 		printf("Sorry, the row you enterred was incorrect. Please enter coordinates again.\n");
 		getPlayerMove(playerMove);
 	}
 	
 	/*row is not a double digit*/
-	if((!(playerMove[2] >= '0' && playerMove[2] <= '9'))){
+	/*if((!(playerMove[2] >= '0' && playerMove[2] <= '9'))){
 		printf("Sorry, the row you enterred was incorrect. Please enter coordinates again.\n");
 		getPlayerMove(playerMove);
-	}
+	}*/
 	return;
 }
 
